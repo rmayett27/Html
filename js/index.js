0f7a4a09fc7c3fe5b21bf8f31c61822ecac5af2c
rmayett27 <rmayett27@hotmail.com>
2015-06-17 22:21:38 -0500

    Proyecto

--- js/index.js
+++ js/index.js
@@ -52,14 +52,14 @@
 			return false
 	}
 	$(document).ready(function(){
-		$("#mostrar").on( "click", function() {
-			$('#target').show(); //muestro mediante id
-			$('.target').show(); //muestro mediante clase
-		 });
 		$("#ocultar").on( "click", function() {
 			$('#target').hide(); //oculto mediante id
 			$('.target').hide(); //muestro mediante clase
 		});
+		$("#mostrar").on( "click", function() {
+			$('#target').show(); //muestro mediante id
+			$('.target').show(); //muestro mediante clase
+		 });		
 	});
 	
 
