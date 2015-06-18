cf1c1253acf06d11b0c9d252aed439e9ad628b97
rmayett27 <rmayett27@hotmail.com>
2015-06-17 22:06:41 -0500

    proyecto

--- js/index.js
+++ js/index.js
@@ -49,7 +49,17 @@
 			$li.val('');
 			return false
 	}
-		
+	$(document).ready(function(){
+		$("#mostrar").on( "click", function() {
+			$('#target').show(); //muestro mediante id
+			$('.target').show(); //muestro mediante clase
+		 });
+		$("#ocultar").on( "click", function() {
+			$('#target').hide(); //oculto mediante id
+			$('.target').hide(); //muestro mediante clase
+		});
+	});
+	
 
 $('#isesion a').click(fIsesion);
 $('#addpost a').click(faddpost);
